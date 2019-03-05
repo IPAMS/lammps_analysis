f0910077e88eb6ba1a8c279bbf0c4f73907e9682
Walter <wissdorf@uni-wuppertal.de>
2019-03-05 16:53:47 +0100

    Restructures the parsing of "thermo_style" output in LAMMPS logs

--- log.py
+++ log.py
@@ -4,36 +4,74 @@ import numpy as np
 import re
 
 
-def parse_thermo_log(filename):
+def parse_run_logs(filename):
 	"""
-	Parses the "thermo" part of a LAMMPS log.
-	The structure of the "thermo" table in a LAMMPS log parsed and its structure
-	and values are returned in a tuple
+	Parses the run information in a LAMMPS log.
+
+	LAMMPS Log files can contain multiple runs, every run starts with "run ntimesteps",
+	some initalization information and lines written to the logfile by the "thermo" fix of LAMMPS
+
+	Currently the thermodynamic data of the, possibly multiple, runs exported by LAMMPS is parsed
+	and returned as list with the data from the individual runs
 
 	:param filename: the name of the logfile to parse
 	:type filename: str
-	:return: a tuple with the (headers of the columns, the times of the timesteps
-	and the values of the thermo table
-	:rtype: tuple: (list of str, np-array of float, 2 dimensional np-array with values)
+	:return: a list, one entry per run, each run is a dictionary with the headers of the columns, the times of the timesteps
+	and the values of the data exported by the thermo_style fix
+	:rtype: list: (list of dict with str, np-array of float, 2 dimensional np-array with values each)
 	"""
-	pt_number_line = re.compile(r"""\s+(?:\-?\d+\.?\d*\s+){10}\n""")
-	pt_header_line = re.compile(r"""(?:\w+ ){10}\n""")
-	timesteps = []
-	values = []
+	pt_header_line = re.compile(r"""Step (?:\w+ )*\n""")
+
+	result = []
 	with open(filename) as fh:
+
+		n_params = -1  # number of parameters in the thermo_style of the current run, -1 if no run was seen yet
+		headers = None # list of strings: the names of the individual colums of the thermo_style
+		pt_number_line = None
+		timesteps = None
+		values = None
+		flag_check_header = False
+
 		for line in fh:
-			match_number = pt_number_line.match(line)
-			if match_number:
-				vals = line.split()
-				timesteps.append(int(vals[0]))
-				values.append([float(vals[i]) for i in range(1, len(vals))])
+
+			if n_params != -1:
+				match_number = pt_number_line.match(line)
+				if match_number:
+					vals = line.split()
+					timesteps.append(int(vals[0]))
+					values.append([float(vals[i]) for i in range(1, len(vals))])
+
+				else:
+					flag_check_header = True
 			else:
+				flag_check_header = True
+
+			if flag_check_header:
+				flag_check_header = False
 				match_header = pt_header_line.match(line)
-				if match_header:
-					headers = line.split()
 
-	timesteps = np.array(timesteps)
-	values = np.array(values)
+				if match_header: # a new thermo_style header was found: prepare the parsing of a new run
+					if n_params != -1: #this is not the first run we have found, store the found data
+						result.append({
+							"headers":headers,
+							"timesteps":np.array(timesteps),
+							"values":np.array(values)})
 
-	return (headers[1:], timesteps, values)
+					#now prepare the new run:
+					headers = line.split()[1:]
+					n_params = len(headers)
 
+					# regex to parse the numbers of the thermo_style definition
+					pt_number_line = re.compile(r"""\s+(?:\-?\d+\.?\d*(?:e[+-]\d+)?\s+){""" + str(n_params+1) + """}\n""") #+1 for steps column
+
+					timesteps = []
+					values = []
+
+		#if the file is parsed to the end and there is a run open, store the found data:
+		if n_params != -1:
+			result.append({
+				"headers": headers,
+				"timesteps": np.array(timesteps),
+				"values": np.array(values)})
+
+	return (result)
--- test/test_log.py
+++ test/test_log.py
@@ -3,13 +3,40 @@
 import unittest
 import lammps_analysis.log as llog
 
-
 class Test_log(unittest.TestCase):
 
-	def test_parse_log(self):
-		headers,ts,vals = llog.parse_thermo_log('log.lammps')
-		self.assertEqual(headers[0],'TotEng')
-		self.assertEqual(headers[5], 'Volume')
-		self.assertEqual(ts[-2],100000)
-		self.assertAlmostEqual(vals[3,0], -10203.461)
+	def test_parse_basic_log(self):
+		runs = llog.parse_run_logs('logs/log.lammps')
 
+		self.assertEqual(len(runs),3)
+
+		self.assertEqual(runs[0]['headers'],['Temp','E_pair','E_mol','TotEng','Press'])
+
+		self.assertEqual(runs[1]['headers'],
+		                 ['TotEng', 'KinEng', 'Density', 'Temp', 'Press', 'Volume', 'pot_drop', 'pot_nitr', 'kin_drop'])
+		self.assertEqual(runs[1]['timesteps'][-2], 10000)
+
+		self.assertEqual(runs[2]['headers'],
+		                 ['TotEng', 'KinEng', 'Density', 'Temp', 'Press', 'Volume', 'pot_drop', 'pot_nitr', 'kin_drop'])
+		self.assertEqual(runs[2]['timesteps'][-2],100000)
+		self.assertAlmostEqual(runs[2]['values'][1,1], 8146.8869)
+
+
+	def test_parse_bigcut_log(self):
+		runs = llog.parse_run_logs('logs/log_counterion_bigcut.lammps')
+
+		self.assertEqual(len(runs),2)
+
+		self.assertEqual(runs[1]['timesteps'][0], 1000)
+		self.assertEqual(runs[1]['timesteps'][-2], 100900)
+
+		self.assertAlmostEqual(runs[1]['values'][3, 3], 8.5079897e-05)
+
+
+	def test_parse_bigcut_stripped_log(self):
+		runs = llog.parse_run_logs('logs/log_counterion_bigcut_stripped.lammps')
+
+		self.assertEqual(len(runs),1)
+		self.assertEqual(runs[0]['headers'], ['Temp', 'E_pair', 'E_mol', 'TotEng', 'Press'])
+		self.assertEqual(runs[0]['timesteps'][0], 0)
+		self.assertEqual(runs[0]['timesteps'][1], 1000)
