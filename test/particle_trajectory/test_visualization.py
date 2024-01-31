60183c257b0119c390d791d998386e23532f8c49
Michelle Rajkovic <michelle.rajkovic@uni-wuppertal.de>
2024-01-31 09:40:19 +0100

    Changes visualization tests to new file format

--- test/particle_trajectory/test_visualization.py
+++ test/particle_trajectory/test_visualization.py
@@ -11,12 +11,12 @@ class Test_analysis(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		data_base_path = pt.join('test', 'testfiles')
-		test_trajectory_path = pt.join(data_base_path, 'trajectories', 'test_trajectory_short.lammpstrj')
+		test_trajectory_path = pt.join(data_base_path, 'trajectories', 'new_format_test_trajectory.lammpstrj')
 
 		cls.test_result_path = pt.join('test', 'testresults')
 
 		cls.trajectory = ltra.read_trajectory(test_trajectory_path)
-		cls.trajectory_water = ltra.filter_species_trajectory(cls.trajectory, [2, 3])
+		cls.trajectory_water = ltra.filter_species_trajectory(cls.trajectory, [63, 64])
 
 	def test_energy_histogram(self):
 		frame = self.trajectory_water[3]
@@ -33,15 +33,11 @@ class Test_analysis(unittest.TestCase):
 
 	def test_radial_density(self):
 		tra = self.trajectory
-		tra_oxygen = ltra.filter_species_trajectory(tra, [2])
-		tra_hydrogen = ltra.filter_species_trajectory(tra, [3])
-		tra_ammonium = ltra.filter_species_trajectory(tra, [350])
-		tra_methanole = ltra.filter_species_trajectory(tra, [100, 101, 102, 99])
+		tra_oxygen = ltra.filter_species_trajectory(tra, [63])
+		tra_hydrogen = ltra.filter_species_trajectory(tra, [64])
 
 		trajectories = [(tra_hydrogen, 'Hydrogen', True),
-		                (tra_oxygen, 'Oxygen', True),
-		                (tra_methanole, 'Methanole', True),
-		                (tra_ammonium, 'Ammonium', True)]
+		                (tra_oxygen, 'Oxygen', True)]
 
 		# radial density plot of whole averaged trajectory:
 		lvisu.plot_radial_density(trajectories, bins=np.linspace(0, 100, 100))
