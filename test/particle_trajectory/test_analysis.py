9a9227870a62762dbbdf7e26acf5a0a0144439e9
Michelle Rajkovic <michelle.rajkovic@uni-wuppertal.de>
2024-01-31 09:34:31 +0100

    Changes analysis test to use new format file

--- test/particle_trajectory/test_analysis.py
+++ test/particle_trajectory/test_analysis.py
@@ -10,10 +10,10 @@ class Test_analysis(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		data_base_path = os.path.join('test', 'testfiles')
-		test_trajectory_path = os.path.join(data_base_path, 'trajectories', 'test_trajectory_short.lammpstrj')
+		test_trajectory_path = os.path.join(data_base_path, 'trajectories', 'new_format_test_trajectory.lammpstrj')
 
 		cls.trajectory = ltra.read_trajectory(test_trajectory_path)
-		cls.trajectory_water = ltra.filter_species_trajectory(cls.trajectory, [2, 3])
+		cls.trajectory_water = ltra.filter_species_trajectory(cls.trajectory, [63, 64])
 		n_particles = 5
 		cls.test_frame = cls.trajectory_water[0].loc[:n_particles,:].copy(deep=True)
 		for i in range(n_particles):
