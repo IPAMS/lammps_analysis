Fixes reproduction skip in averaging function

src/LammpsAnalysis/cluster/simulation_evaluation.py
@@ -82,7 +82,6 @@ def eval_cluster_mean(index):
 
     def cluster_mean(data):
         nonlocal index
-        print(data)
         for variation_index, reprod_sum in enumerate(data[index,:]):
             stddev = np.std(reprod_sum)
             count = np.mean(reprod_sum)
@@ -95,7 +94,7 @@ def eval_cluster_mean_after_collision(index_cluster, index_collision):
         nonlocal index_cluster
         nonlocal index_collision
         
-        collision_point = np.mean(data[index_collision,:])
+        collision_point =np.round(np.mean(data[index_collision,:]))
         for variation_index, reprod_sum in enumerate(data[index_cluster,:]):
             stddev = np.std(reprod_sum[collision_point:])
             count = np.mean(reprod_sum[collision_point:])
@@ -132,71 +131,20 @@ def average_observables(filenames, functions, post_functions, reproduction_count
         data = cl.read_cluster_data(file, frames_to_read)
         for index, function in enumerate(functions):
             function(data, function_results, index, file_res_counter)
-            
-            repro_counter = repro_counter+1
-            if repro_counter >= reproduction_count:
+            print(counter, index, function_results[index,file_res_counter])
+        repro_counter = repro_counter+1 
+
+        if repro_counter >= reproduction_count:
                 repro_counter = 0
                 for id, element in enumerate(function_results[:, file_res_counter]):
-                    print(id, element, function_results[id, file_res_counter])
                     function_results[id, file_res_counter] = element/reproduction_count
+                    print(element, function_results[id, file_res_counter])
                 file_res_counter = file_res_counter + 1
     
     for postindex, postfunction in enumerate(post_functions):           
         postfunction(function_results)
 
     return function_results
-
-
-# def evaluate_simulation_run(filenames, functions, post_functions, frames_to_read, reproduction_count, wall_type):
-#     function_count = len(functions)
-#     file_count = len(filenames)
-#     post_functions_count = len(post_functions)
-#     function_results = np.empty((function_count,file_count//reproduction_count), dtype=object)
-
-#     repro_counter = 0
-#     file_res_counter = 0
-#     for counter, file in enumerate(filenames): 
-#         data = cl.read_cluster_data(file, frames_to_read)
-#         for index, function in enumerate(functions):
-#             if function == collision_point:
-#                 result = function(data, wall_type)
-#             else:
-#                 result = function(data)
-#             if np.any(function_results[index, file_res_counter]):
-#                 function_results[index, file_res_counter] = function_results[index, file_res_counter] + result
-#             else:
-#                 function_results[index, file_res_counter] = result
-                
-#             repro_counter = repro_counter+1
-#             if repro_counter >= reproduction_count:
-#                 repro_counter = 0
-#                 for id, element in enumerate(function_results[:, file_res_counter]):
-#                     function_results[id, file_res_counter] = element/reproduction_count
-#                 file_res_counter = file_res_counter + 1
-    
-#     for postindex, postfunction in enumerate(post_functions):           
-#         postfunction(function_results)
-
-#     return function_results
-
-# def evaluate_timeframe_sums(filenames, functions, post_functions, wall_type, timestep, frames_to_read=None):
-#     function_count = len(functions)
-#     function_results = np.empty((function_count,1), dtype=object)
-
-#     for counter, file in enumerate(filenames): 
-#         data = cl.read_cluster_data(file, frames_to_read)
-#         for index, function in enumerate(functions):
-           
-#             result = function(data, timestep)
-#             if np.any(function_results[index,0]):
-#                 function_results[index,0] = pd.concat([function_results[index,0], result]).groupby('cluster')['occurence'].sum().reset_index()
-#             else:
-#                 function_results[index,0] = result   
-
-#     for postindex, postfunction in enumerate(post_functions):           
-#         postfunction(function_results)
-    
-#     return function_results
 
 
 def plot_cluster_count(data, index, voltages):